4d3b2907886410e7500c46079a10bdfd5981e35b
Alwinfahrozi <alwin.marbun123@gmail.com>
2025-03-17 03:15:34 +0800

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ MOD5_1302204032_KPL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericExample
+{
+    // Membuat class HaloGeneric
+    public class HaloGeneric
+    {
+        // Method dengan generic parameter
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+
+        // Method utama (main) untuk memanggil SapaUser
+        public static void Main(string[] args)
+        {
+            // Membuat objek dari class HaloGeneric
+            HaloGeneric halo = new HaloGeneric();
+
+            // Memanggil method SapaUser dengan input nama praktikan
+            halo.SapaUser("Alwin Fahrozi Marbun");
+        }
+    }
+}
